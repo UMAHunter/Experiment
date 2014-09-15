f2d9192dca623c37e32c5302fd74c58cc482bc9b
UMAHunter <fandeyuan123@163.com>
2014-09-15 19:56:48 +0800

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,28 +1,27 @@
 
 /**************************************************************************************************/
-/* Copyright (C) mc2lab.com, SSE@USTC, 2014-2015                                                  */
+/* Copyright (C) SSE@USTC, 2014-2015                                                              */
 /*                                                                                                */
 /*  FILE NAME             :  menu.c                                                               */
-/*  PRINCIPAL AUTHOR      :  Mengning                                                             */
+/*  PRINCIPAL AUTHOR      :  Fandeyuan                                                            */
 /*  SUBSYSTEM NAME        :  menu                                                                 */
 /*  MODULE NAME           :  menu                                                                 */
 /*  LANGUAGE              :  C                                                                    */
 /*  TARGET ENVIRONMENT    :  ANY                                                                  */
-/*  DATE OF FIRST RELEASE :  2014/08/31                                                           */
+/*  DATE OF FIRST RELEASE :  2014/09/15                                                           */
 /*  DESCRIPTION           :  This is a menu program                                               */
 /**************************************************************************************************/
 
 /*
  * Revision log:
  *
- * Created by Mengning, 2014/08/31
+ * Created by Fandeyuan, 2014/09/15
  *
  */
 
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "linktable.h"
 
 int Help();
 
@@ -30,82 +29,56 @@ int Help();
 #define DESC_LEN    1024
 #define CMD_NUM     10
 
-/* data struct and its operations */
-
 typedef struct DataNode
 {
-    tLinkTableNode * pNext;
     char*   cmd;
     char*   desc;
     int     (*handler)();
+    struct  DataNode *next;
 } tDataNode;
 
-/* find a cmd in the linklist and return the datanode pointer */
-tDataNode* FindCmd(tLinkTable * head, char * cmd)
+static tDataNode head[] = 
 {
-    tDataNode * pNode = (tDataNode*)GetLinkTableHead(head);
-    while(pNode != NULL)
-    {
-        if(!strcmp(pNode->cmd, cmd))
-        {
-            return  pNode;  
-        }
-        pNode = (tDataNode*)GetNextLinkTableNode(head,(tLinkTableNode *)pNode);
-    }
-    return NULL;
-}
-
-/* show all cmd in listlist */
-int ShowAllCmd(tLinkTable * head)
-{
-    tDataNode * pNode = (tDataNode*)GetLinkTableHead(head);
-    while(pNode != NULL)
-    {
-        printf("%s - %s\n", pNode->cmd, pNode->desc);
-        pNode = (tDataNode*)GetNextLinkTableNode(head,(tLinkTableNode *)pNode);
-    }
-    return 0;
-}
-
-/* menu program */
-
-static tDataNode data[] = 
-{
-    {NULL, "help", "this is help cmd!", Help},
-    {NULL, "version", "menu program v1.0", NULL}
+    {"help", "this is help cmd!", Help,&head[1]},
+    {"version", "menu program v1.0", NULL, NULL}
 };
-
-tLinkTable * head = NULL;
-
 main()
 {
-    head = CreateLinkTable();
-    AddLinkTableNode(head,(tLinkTableNode *)&data[0]);
-    AddLinkTableNode(head,(tLinkTableNode *)&data[1]);
-    
    /* cmd line begins */
     while(1)
     {
         char cmd[CMD_MAX_LEN];
         printf("Input a cmd number > ");
         scanf("%s", cmd);
-        tDataNode *p = FindCmd(head, cmd);
-        if( p == NULL)
+        tDataNode *p = head;
+        while(p != NULL)
+        {
+            if(!strcmp(p->cmd, cmd))
+            {
+                printf("%s - %s\n", p->cmd, p->desc);
+                if(p->handler != NULL)
+                {
+                    p->handler();
+                }
+                break;
+            }
+            p = p->next;
+        }
+        if(p == NULL) 
         {
             printf("This is a wrong cmd!\n ");
-            continue;
         }
-        printf("%s - %s\n", p->cmd, p->desc); 
-        if(p->handler != NULL) 
-        { 
-            p->handler();
-        }
-   
     }
 }
 
 int Help()
 {
-    ShowAllCmd(head);
+    printf("Menu List:\n");
+    tDataNode *p = head;
+    while(p != NULL)
+    {
+        printf("%s - %s\n", p->cmd, p->desc);
+        p = p->next;
+    }
     return 0; 
 }
